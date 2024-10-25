5f3c57cd887d4b367c56bfcaedb006246541c8d0
Vic <thangson62@gmail.com>
2024-10-25 16:06:56 +0700

    refix LoginPage

--- fashion-web-client/src/page/LoginPage/index.jsx
+++ fashion-web-client/src/page/LoginPage/index.jsx
@@ -1,10 +1,44 @@
-import React, { useState } from 'react';
-import Carousel from 'react-grid-carousel'; // Import Carousel
+import React, { useEffect, useRef, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react'; // Import Swiper
+import '../../../node_modules/swiper/swiper-bundle.min.css';
 import LoginForm from './LoginForm';
 import SignUpForm from './SignUpForm';
 
 function LoginPage(props) {
     const [isSignup, setIsSignUp] = useState(false);
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const swiperRef = useRef(null);
+    const slides = [
+        require('../../assets/img/hero-image.png'),
+        require('../../assets/img/img1.jpg'),
+        require('../../assets/img/img2.jpg'),
+        require('../../assets/img/img3.jpg'),
+        require('../../assets/img/img4.jpg'),
+    ];
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            // Tăng chỉ số hiện tại
+            setCurrentIndex((prevIndex) => {
+                if (prevIndex === slides.length - 1) {
+                    // Nếu đang ở slide cuối cùng, quay lại slide đầu tiên
+                    return 0;
+                } else {
+                    // Chuyển đến slide tiếp theo
+                    return prevIndex + 1;
+                }
+            });
+        }, 3000); // Chuyển đổi mỗi 3 giây
+
+        return () => clearInterval(interval); // Dọn dẹp khi component unmount
+    }, [slides.length]);
+
+    useEffect(() => {
+        if (swiperRef.current) {
+            // Chuyển đến slide hiện tại
+            swiperRef.current.swiper.slideTo(currentIndex);
+        }
+    }, [currentIndex]);
 
     return (
         <div style={{ marginTop: '25px' }}>
@@ -15,45 +49,17 @@ function LoginPage(props) {
             </section>
             <div className='loginform'>
                 <div className='container-1139 loginform-container'>
-                    <Carousel
-                        hideArrow={true}
-                        cols={1}
-                        rows={1}
-                        containerStyle={{ maxWidth: '40.65%' }}
-                        loop
-                        autoplay={3000}
+                    <Swiper
+                        ref={swiperRef}
+                        spaceBetween={30} // Khoảng cách giữa các slide
+                        loop={false} // Tắt chế độ lặp lại của Swiper
                     >
-                        <Carousel.Item>
-                            <img
-                                src={require('../../assets/img/hero-image.png')}
-                                alt='login'
-                            />
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img
-                                src={require('../../assets/img/img1.jpg')}
-                                alt='login'
-                            />
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img
-                                src={require('../../assets/img/img2.jpg')}
-                                alt='login'
-                            />
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img
-                                src={require('../../assets/img/img3.jpg')}
-                                alt='login'
-                            />
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img
-                                src={require('../../assets/img/img4.jpg')}
-                                alt='login'
-                            />
-                        </Carousel.Item>
-                    </Carousel>
+                        {slides.map((slide, index) => (
+                            <SwiperSlide key={index}>
+                                <img src={slide} alt='login' />
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
                     {isSignup
                         ? <SignUpForm setIsSignUp={setIsSignUp} setUser={props.setUser} />
                         : <LoginForm setIsSignUp={setIsSignUp} />
--- fashion-web-client/src/page/PurchasedPage/index.jsx
+++ fashion-web-client/src/page/PurchasedPage/index.jsx
@@ -1,30 +1,30 @@
-import Header from '../../components/Header';
-import Logo from '../../components/Logo';
-import Address from './address';
-import Category from './category';
-import Item from './item'
-import Total from './total';
-import Footer from '../../components/Footer';
-import { useSelector } from 'react-redux';
-import { getItemsInCart, getUser } from '../../redux/selectors';
+// import Header from '../../components/Header';
+// import Logo from '../../components/Logo';
+// import Address from './address';
+// import Category from './category';
+// import Item from './item'
+// import Total from './total';
+// import Footer from '../../components/Footer';
+// import { useSelector } from 'react-redux';
+// import { getItemsInCart, getUser } from '../../redux/selectors';
 
 
-function PurchasedPage() {
-    const user = useSelector(getUser);
-    const itemsInCart = useSelector(getItemsInCart);
+// function PurchasedPage() {
+//     const user = useSelector(getUser);
+//     const itemsInCart = useSelector(getItemsInCart);
 
 
-    return (
-        <div style={{ backgroundColor: 'var(--gray-color)' }}>
-            <Header user={user} />
-            <Logo location='Thanh toán' />
-            <Address user={user} />
-            <Category />
-            {itemsInCart.map(item => { return <Item item={item} />;})}
-            <Total itemsInCart={itemsInCart} />
-            <Footer />
+//     return (
+//         <div style={{ backgroundColor: 'var(--gray-color)' }}>
+//             <Header user={user} />
+//             <Logo location='Thanh toán' />
+//             <Address user={user} />
+//             <Category />
+//             {itemsInCart.map(item => { return <Item item={item} />;})}
+//             <Total itemsInCart={itemsInCart} />
+//             <Footer />
             
-        </div>
-    );
-}
-export default PurchasedPage;
+//         </div>
+//     );
+// }
+// export default PurchasedPage;
