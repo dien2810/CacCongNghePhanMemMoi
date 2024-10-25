2cfdd1b408f8fa2369d6f92a47f1fb503e204176
Vic <thangson62@gmail.com>
2024-10-25 13:19:02 +0700

    Cập nhật file và thêm file mới

---
+++ fashion-web-client/src/components/Loading.jsx
@@ -0,0 +1,13 @@
+import '../assets/css/Loading.css';
+
+function Loading() {
+	return (
+		<div className='uiewr'>
+			<div className='loading-container'>
+				<div className='loader'></div>
+			</div>
+		</div>
+	);
+}
+
+export default Loading;
--- fashion-web-client/src/page/PurchasedPage/index.jsx
+++ fashion-web-client/src/page/PurchasedPage/index.jsx
@@ -1,8 +1,30 @@
-function PurchasedPage(props) {
-  return (
-    <div style={{ backgroundColor: "white", marginTop: "25px" }}>
-      <h1>PurchasedPage</h1>
-    </div>
-  );
+import Header from '../../components/Header';
+import Logo from '../../components/Logo';
+import Address from './address';
+import Category from './category';
+import Item from './item'
+import Total from './total';
+import Footer from '../../components/Footer';
+import { useSelector } from 'react-redux';
+import { getItemsInCart, getUser } from '../../redux/selectors';
+
+
+function PurchasedPage() {
+    const user = useSelector(getUser);
+    const itemsInCart = useSelector(getItemsInCart);
+
+
+    return (
+        <div style={{ backgroundColor: 'var(--gray-color)' }}>
+            <Header user={user} />
+            <Logo location='Thanh toán' />
+            <Address user={user} />
+            <Category />
+            {itemsInCart.map(item => { return <Item item={item} />;})}
+            <Total itemsInCart={itemsInCart} />
+            <Footer />
+            
+        </div>
+    );
 }
 export default PurchasedPage;
