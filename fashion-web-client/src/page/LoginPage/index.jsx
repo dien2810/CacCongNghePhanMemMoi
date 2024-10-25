af1c69bf3ef133ac7958543398219ab555fec0f9
Vic <thangson62@gmail.com>
2024-10-25 13:19:02 +0700

    refix LoginPage

--- fashion-web-client/src/page/LoginPage/index.jsx
+++ fashion-web-client/src/page/LoginPage/index.jsx
@@ -1,8 +1,67 @@
+import React, { useState } from 'react';
+import Carousel from 'react-grid-carousel'; // Import Carousel
+import LoginForm from './LoginForm';
+import SignUpForm from './SignUpForm';
+
 function LoginPage(props) {
-  return (
-    <div style={{ backgroundColor: "white", marginTop: "25px" }}>
-      <h1>LoginPage</h1>
-    </div>
-  );
+    const [isSignup, setIsSignUp] = useState(false);
+
+    return (
+        <div style={{ marginTop: '25px' }}>
+            <section>
+                <div className='container-1139'>
+                    <h1 className='logo'>Login</h1>
+                </div>
+            </section>
+            <div className='loginform'>
+                <div className='container-1139 loginform-container'>
+                    <Carousel
+                        hideArrow={true}
+                        cols={1}
+                        rows={1}
+                        containerStyle={{ maxWidth: '40.65%' }}
+                        loop
+                        autoplay={3000}
+                    >
+                        <Carousel.Item>
+                            <img
+                                src={require('../../assets/img/hero-image.png')}
+                                alt='login'
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                src={require('../../assets/img/img1.jpg')}
+                                alt='login'
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                src={require('../../assets/img/img2.jpg')}
+                                alt='login'
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                src={require('../../assets/img/img3.jpg')}
+                                alt='login'
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                src={require('../../assets/img/img4.jpg')}
+                                alt='login'
+                            />
+                        </Carousel.Item>
+                    </Carousel>
+                    {isSignup
+                        ? <SignUpForm setIsSignUp={setIsSignUp} setUser={props.setUser} />
+                        : <LoginForm setIsSignUp={setIsSignUp} />
+                    }
+                </div>
+            </div>
+        </div>
+    );
 }
+
 export default LoginPage;
